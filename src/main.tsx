98d7561c719b573efa239c2a2b6702e95bf61bdc
gervasiomeana-arg <gervasiomeana@gmail.com>
2026-09-21 17:20:32 -0300

    Activar recuperación global de errores

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {AppErrorBoundary} from './components/common/AppErrorBoundary.tsx';
 import {RemoteAuthGate} from './components/common/RemoteAuthGate.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RemoteAuthGate><App /></RemoteAuthGate>
+    <AppErrorBoundary>
+      <RemoteAuthGate><App /></RemoteAuthGate>
+    </AppErrorBoundary>
   </StrictMode>,
 );
